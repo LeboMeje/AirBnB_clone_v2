d13c902abdcb48eb28cd654bcb5501a447f6af46
LeboMeje <lebomhopewell@gmail.com>
2023-08-18 23:04:17 +0000

    adding python scripts that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,10 @@
 """
 script starts Flask web app
     listen on 0.0.0.0, port 5000
-    routes: /: display "Hello HBNB!"
+    routes: /airbnb-onepage/: display "Hello HBNB!"
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("__name__")
 
@@ -13,8 +13,8 @@ app = Flask("__name__")
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """display text"""
-    return "Hello HBNB!"
+    return render_template("100-hbnb.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True
